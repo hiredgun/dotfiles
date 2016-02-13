cbc4faca095248c56c0b77e1a09b91d660db5df9
Konrad Turczyński <konrad.turczynski@schibsted.pl>
2016-02-13 21:00:34 +0100

    Add new aliases

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -1,6 +1,8 @@
+alias cll='clear && ls -lG'
 alias ll='ls -lG'
 alias cc='clear'
 alias la='ls -laG'
+alias cla='clear && ls -laG'
 alias pubkey="more ~/.ssh/id_rsa.pub | pbcopy | echo '=> Public key copied to pasteboard.'"
 alias cc='clear'
 alias specr='vendor/bin/phpspec run --config=tests/spec/phpspec.yml.dist  --format=pretty --ansi --verbose'
